d9550bf3674e2fc34346f5607488b85e6b7a993b
Scott Hamilton <scott.hamilton@dockyard.com>
2020-06-26 15:48:11 -0500

    WIP Handles nodes joining and leaving at the same time Hacky to have a sleep in case of a race condition

--- lib/ex_cluster/ex_cluster/order.ex
+++ lib/ex_cluster/ex_cluster/order.ex
@@ -2,6 +2,8 @@ defmodule ExCluster.Order do
   use GenServer
   require Logger
 
+  alias ExCluster.StateHandoff
+
   def child_spec(customer), do: %{id: customer, start: {__MODULE__, :start_link, [customer]}}
 
   def start_link(customer) do
@@ -23,7 +25,19 @@ defmodule ExCluster.Order do
     {:via, Horde.Registry, {ExCluster.OrderRegistry, customer}}
   end
 
-  def init(customer), do: {:ok, {customer, []}}
+  def init(customer) do
+    Process.flag(:trap_exit, true)
+    order_contents = StateHandoff.load(customer)
+
+    {:ok, {customer, order_contents}}
+  end
+
+  def terminate(reason, {customer, order_contents}) do
+    # save state in storage layer
+    ExCluster.StateHandoff.save(customer, order_contents)
+
+    :ok
+  end
 
   def handle_cast({:add, new_order_contents}, {customer, order_contents}) do
     {:noreply, {customer, order_contents ++ new_order_contents}}
--- lib/ex_cluster/ex_cluster/order_supervisor.ex
+++ lib/ex_cluster/ex_cluster/order_supervisor.ex
@@ -3,16 +3,17 @@ defmodule ExCluster.OrderSupervisor do
 
   def start_link(init_arg \\ []) do
     init_arg =
-    [
-      strategy: :one_for_one,
-      distribution_strategy: Horde.UniformQuorumDistribution,
-      max_restarts: 100_000,
-      max_seconds: 1,
-      shutdown: 50_000,
-      members: members()
-    ]
-    |> Keyword.merge(init_arg)
-    Horde.DynamicSupervisor.start_link(__MODULE__, init_arg, [name: __MODULE__])
+      [
+        strategy: :one_for_one,
+        distribution_strategy: Horde.UniformQuorumDistribution,
+        max_restarts: 100_000,
+        max_seconds: 1,
+        shutdown: 50_000,
+        members: members()
+      ]
+      |> Keyword.merge(init_arg)
+
+    Horde.DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 
   def init(init_arg) do
